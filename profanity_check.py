38a39087845131246a0ddf2e01eee29e0a105f01
wh1z57 <shubham9341@gmail.com>
2022-02-18 00:52:41 +0530

    added some more comments

--- profanity_check.py
+++ profanity_check.py
@@ -1,26 +1,26 @@
 import os
 dirname = os.path.dirname(__file__)
-profanityList = os.path.join(dirname, 'profanity.txt')
+profanity_list = os.path.join(dirname, 'profanity.txt')
 tweets=os.path.join(dirname, 'tweets.txt')
-profanityWords=[]
+profanity_words=[]
 degree={1:'Low',2:'High'} #dictionary to 
 
-def checkProfanity(word): #function to check if a word from the tweet is in the list of racial words list
-    if(any(word in slur for slur in profanityWords) ):
+def check_profanity(word): #function to check if a word from the tweet is in the list of racial words list
+    if(any(word in slur for slur in profanity_words) ):
                 return 1      
 
-with open(profanityList,'r') as file:     #get words from the racial slurs list and store it in the list 'profanityList'
+with open(profanity_list,'r') as file:     #get words from the racial slurs list and store it in the list 'profanity_list'
     for line in file:      
         for word in line.split():       
-            profanityWords.append(word.lower()) 
+            profanity_words.append(word.lower()) 
 with open(tweets,'r') as file:    #read the tweets file,assuming each tweet is in a single line
     count=0
     for line in file:      
         for word in line.split():
-            if(checkProfanity(word)):   #check for word in the list "profanityWords"
+            if(check_profanity(word)):   #check for word in the list "profanity_words"
                 count+=1    
         if(count>2):  #check for degree of profanity
             print('Profanity Degree in the tweet:',line.strip(),'is Severe')
         elif(count>0 and count<=2):
-            print('Profanity Degree in the tweet:',line.strip(),'is',degree[count],count)
+            print('Profanity Degree in the tweet:',line.strip(),'is',degree[count])
         count=0 #reset count after every tweet
